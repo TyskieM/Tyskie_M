ab2e1ac48029d49abbf5138b511c002bc5ee8a04
TyskieM <65892726+TyskieM@users.noreply.github.com>
2020-05-26 12:53:59 +0200

    main

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'NzA0OTk3MDg5MzIyNTk4NDUw.XsvXNA.6w1kJrZqr2kKxsvTTQdYjY8Diss';
+
+const PREFIX = '!';
+
+bot.on('ready', MessageChannel=>{
+    console.log('bot został uruchomiony.')
+})
+
+bot.on('message', message=>{
+
+
+let args = message.content.substring(PREFIX.lenght).split(" ");
+    switch(args[0]){
+        case 'witaj':
+            message.channel.send('Witaj, witam cie na discord :D');
+        break;
+
+        case 's!clear':
+            if(!message.member.hasPermission('MANAGE_MESSAGES')){
+                message.channel.send('Nie masz permisji')
+                return;
+
+            }
+            message.channel.bulkDelete(args[1])
+        break;
+        }
+
+})
+
+bot.login(token);
